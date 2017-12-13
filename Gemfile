dcec2d5984484d5677365a304df8ba591edbb98d
linuxcat <sarndeepnijjar@googlemail.com>
2017-12-13 20:29:08 +0000

    simple inf tests

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'serverspec'
+gem 'docker-api'
+gem 'rspec_html_formatter'
+gem 'activesupport', '4.2.6'
+gem 'dockerspec', '~> 0.5.0'
+gem 'rspec-core', '< 3.6'
+
---
+++ spec/appserver/docker_compose_spec.rb
@@ -0,0 +1,16 @@
+require 'dockerspec/serverspec'
+
+describe docker_compose('docker-compose-dev.yml', wait: 20, rm: true) do
+
+  its_container(:database) do
+    describe port(5432) do
+      it { should be_listening('tcp') }
+    end
+
+    describe file('/usr/share/postgresql/9.6/postgresql.conf') do
+      it { should exist }
+    end
+
+  end
+
+end
